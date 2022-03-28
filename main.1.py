b78efc01ec9686273114d873baa1bc2f8b17095c
tdOSX <Unaimend@gmail.com>
2022-03-28 14:36:49 +0200

    Added name to our programm

--- main.1.py
+++ main.1.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 
-def func(a, b, c):
-    return a + b + c
-
 def main():
- print("hello from python")
+ name = input("Please input your name" )
+ print(f"hello from python{name}")
  
  
 if __name__ == "__main__":
  main()
- func(1,2,3)
 
